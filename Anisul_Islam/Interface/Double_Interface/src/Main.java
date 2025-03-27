aca911f1d5978d41e9a732563d1c5e7b2330ab8b
Umar Farouk Ahmed <umarahmed292@gmail.com>
2025-03-27 15:16:32 +0600

    double interface implementation

---
+++ Anisul_Islam/Interface/Double_Interface/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Monitor acer = new Monitor();
+        acer.print();
+        acer.show();
+    }
+}
---
+++ Anisul_Islam/Interface/Double_Interface/src/Monitor.java
@@ -0,0 +1,9 @@
+public class Monitor implements Print,Show{
+    @Override
+    public void print() {
+        System.out.println("Printing data");
+    }
+    public void show(){
+        System.out.println("Showing data");
+    }
+}
